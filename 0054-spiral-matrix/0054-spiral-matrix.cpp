33c7867b918e3e605cc20462d02ff883832b7a47
HIMANSHU SHARMA <himanshu11032006sharma@gmail.com>
2026-08-19 20:46:14 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.69%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int srow = 0;
+        int erow = m - 1;
+        int scol = 0;
+        int ecol = n - 1;
+
+        vector<int> ans;
+
+        while (srow <= erow && scol <= ecol) {
+
+            // Top
+            for (int j = scol; j <= ecol; j++) {
+                ans.push_back(matrix[srow][j]);
+            }
+
+            // Right
+            for (int i = srow + 1; i <= erow; i++) {
+                ans.push_back(matrix[i][ecol]);
+            }
+
+            // Bottom
+            for (int j = ecol - 1; j >= scol; j--) {
+                if (srow == erow) {
+                    break;
+                }
+                ans.push_back(matrix[erow][j]);
+            }
+
+            // Left
+            for (int i = erow - 1; i > srow; i--) {
+                if (scol == ecol) {
+                    break;
+                }
+                ans.push_back(matrix[i][scol]);
+            }
+
+            srow++;
+            erow--;
+            scol++;
+            ecol--;
+        }
+
+        return ans;
+    }
+};
